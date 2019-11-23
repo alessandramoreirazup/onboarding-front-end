ab79e0e3752b07c3498d2abcc36642e98eeddf11
alessandramoreirazup <alessandra.moreira@zup.com.br>
2019-11-23 17:33:55 -0300

    adiciona angular material design e quiz module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { getAllLifecycleHooks } from '@angular/compiler/src/lifecycle_reflector';
 
 @Component({
   selector: 'app-root',
@@ -8,3 +9,4 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'onboarding-front-end';
 }
+
