59ce77f1a494e1e1971c629a8d5a8b84bba254d8
beatrizsilvazup <beatriz.silva@zup.com.br>
2019-12-03 22:16:29 -0300

    adiciona api google funcionando

--- src/modules/login/components/login.component.ts
+++ src/modules/login/components/login.component.ts
@@ -1,16 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AuthService } from "angularx-social-login";
+import { GoogleLoginProvider } from "angularx-social-login";
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
 
-  constructor() { }
-
+  export class LoginComponent implements OnInit {
+ 
+    constructor(private authService: AuthService) { }
+   
+    signInWithGoogle(): void {
+      this.authService.signIn(GoogleLoginProvider.PROVIDER_ID);
+    }
+   
+    signOut(): void {
+      this.authService.signOut();
+    }
+   
+  
   ngOnInit() {
+    // ...
   }
-
 }
--- src/modules/login/login.module.ts
+++ src/modules/login/login.module.ts
@@ -2,20 +2,36 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { LoginComponent } from './components/login.component';
-import { ButtonGoogleComponent } from './button-google/button-google.component';
-import { SocialLoginModule, AuthServiceConfig, GoogleLoginProvider } from 'angularx-social-login';
-import { getAuthServiceConfigs } from './socialloginConfig';
+import { SocialLoginModule, AuthServiceConfig } from "angularx-social-login";
+import { GoogleLoginProvider, LoginOpt } from "angularx-social-login";
 
+function getAuthServiceConfigs(): AuthServiceConfig{
 
+const opt: LoginOpt = {
+  hosted_domain: 'zup.com.br'
+}
+
+let config = new AuthServiceConfig([
+  {
+    id: GoogleLoginProvider.PROVIDER_ID,
+    provider: new GoogleLoginProvider("878581987495-v9mhl31sn2plog3n53fgapuqe8gq72tu.apps.googleusercontent.com", opt),
+  }
+]);
+
+  return config;
+}
 
 @NgModule({
-  declarations: [LoginComponent, ButtonGoogleComponent],
+  declarations: [LoginComponent],
   imports: [
     CommonModule,
     SocialLoginModule
   ],
   providers: [
-    SocialLoginModule.initialize(getAuthServiceConfigs)
+    {
+      provide: AuthServiceConfig,
+      useFactory: getAuthServiceConfigs
+    }
   ]
 })
 export class LoginModule { }
