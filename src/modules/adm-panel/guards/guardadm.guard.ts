67c4f62d1d6417b3b3943bdacdea16c7846bef86
beatrizsilvazup <beatriz.silva@zup.com.br>
2020-01-03 17:14:21 -0300

    adiciona guard adm

--- src/modules/adm-panel/guards/guardadm.guard.ts
+++ src/modules/adm-panel/guards/guardadm.guard.ts
@@ -3,16 +3,12 @@ import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { LoginService } from 'src/modules/login/service/login.service';
-import { userInfo } from 'os';
 
 @Injectable({
   providedIn: 'root'
 })
 
-export class LoginGuard implements CanActivate {
-  AuthService: any;
-  private user: SocialUser;
-  private loggedIn: boolean;
+export class GuardAdmGuard implements CanActivate {
  
   // private isAuthenticated: boolean = false;
 
@@ -29,7 +25,7 @@ export class LoginGuard implements CanActivate {
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
       
-    if (this.loginService.getEmail() == 'bbeamoraes@gmail.com') {
+    if (this.loginService.getEmail()) {
       return true;
     }
 
--- src/modules/adm-panel/service/adm-panel.service.ts
+++ src/modules/adm-panel/service/adm-panel.service.ts
@@ -1,12 +1,32 @@
+import { Router } from '@angular/router';
 import { Injectable } from '@angular/core';
+import { AuthService, SocialUser } from 'angularx-social-login';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdmPanelService {
 
-  constructor() { }
+  private user: SocialUser;
+  private loggedIn: boolean = false;
+ 
+  constructor(private authService: AuthService,
+    private router: Router) { }
+ 
+  getEmail() {
+    this.authService.authState.subscribe((user) => {
+      this.user = user;
+      this.loggedIn = (user != null);
+    });
 
-
-  
+    if(this.user.email === 'beatriz.silva@zup.com'){
+      this.loggedIn = true;
+      this.router.navigate(['/admpanel']);
+      console.log('funcionouu');
+      
+    } else {
+      this.loggedIn = false;
+    }
+  }
+ 
 }
